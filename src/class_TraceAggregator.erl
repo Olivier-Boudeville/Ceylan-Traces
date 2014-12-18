0c370367c105c0e1970b271bce349b994d10388b
Olivier Boudeville <olivier.boudeville@esperide.com>
2014-12-18 18:24:05 +0100

    Absolute paths used now.

--- src/class_TraceAggregator.erl
+++ src/class_TraceAggregator.erl
@@ -132,9 +132,10 @@ construct( State, ?wooper_construct_parameters ) ->
 	% First the direct mother classes (none here), then this class-specific
 	% actions:
 
+	AbsTraceFilename = file_utils:ensure_path_is_absolute( TraceFilename ),
 
 	% Creates the trace file as soon as possible:
-	File = open_trace_file( TraceFilename ),
+	File = open_trace_file( AbsTraceFilename ),
 
 	% Increases the chances that the aggregator does not lag too much behind the
 	% current simulation state:
@@ -142,7 +143,7 @@ construct( State, ?wooper_construct_parameters ) ->
 
 	SetState = setAttributes( State, [
 
-		{ trace_filename, TraceFilename },
+		{ trace_filename, AbsTraceFilename },
 		{ trace_file, File },
 		{ trace_type, TraceType },
 		{ trace_title, TraceTitle },
@@ -154,7 +155,7 @@ construct( State, ?wooper_construct_parameters ) ->
 	Message = io_lib:format( "Trace aggregator created, "
 							 "trace filename is '~s', trace type is '~w', "
 							 "and trace title is '~s'.~n",
-							 [ TraceFilename, TraceType, TraceTitle ] ),
+							 [ AbsTraceFilename, TraceType, TraceTitle ] ),
 
 	TimestampText = text_utils:string_to_binary(
 					  basic_utils:get_textual_timestamp() ),
